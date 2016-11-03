35ea4127ad1a1e3cf9e7c164907e297cc8261b8e
Nicholas Heiner <nicholas_t_heiner@omb.eop.gov>
2016-11-03 17:04:05 -0400

    Adding the start of tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,26 @@
 'use strict';
 
-const test = require('ava');
+const test = require('ava'),
+    path = require('path'),
+    webpack = require('webpack'),
 
-test('Sample test', function(t) {
-    t.pass('Ava is working');
+    fs = require('fs'),
+    
+    packageJson = require('../package'),
+    pathToSassjsLoader = path.join(__dirname, '..', packageJson.main),
+    pathToEntrySass = path.join(__dirname, 'fixtures', 'index.scss');
+
+test('Basic sass', function(t) {
+    t.plan(1);
+    webpack({
+        entry: 'raw!' + pathToSassjsLoader + '!' + pathToEntrySass
+    }, function(err, stats) {
+        if (err) {
+            t.fail(err);
+        }
+
+        const expectedContents = fs.readFileSync(__dirname, 'expected', 'basic.css');
+
+        t.is(stats.toString(), expectedContents);
+    });
 });
