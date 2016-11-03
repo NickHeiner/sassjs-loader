54147d0ab65a651d7f1ac67bb86e94e0132d806c
Nicholas Heiner <nicholas_t_heiner@omb.eop.gov>
2016-11-03 17:24:22 -0400

    Making linting more specific and moving towards a test that works

--- test/index.js
+++ test/index.js
@@ -5,22 +5,27 @@ const test = require('ava'),
     webpack = require('webpack'),
     q = require('q'),
     fs = require('fs'),
+    tmp = require('tmp'),
     
     packageJson = require('../package'),
     pathToSassjsLoader = path.join(__dirname, '..', packageJson.main),
-    pathToEntrySass = path.join(__dirname, 'fixtures', 'index.scss');
+    pathToEntrySass = path.join(__dirname, 'fixtures', 'basic.scss');
 
 test('Basic sass', function(t) {
     t.plan(1);
-    console.log('plan');
 
-    return q.nfcall(webpack, {
-        entry: 'raw!' + pathToSassjsLoader + '!' + pathToEntrySass
-    }).then(function(stats) {
-        const expectedContents = fs.readFileSync(path.join(__dirname, 'expected', 'basic.css'), 'utf8');
+    return q.ninvoke(tmp, 'file')
+        .then(function(filePath) {
+            return q.nfcall(webpack, {
+                entry: 'raw!' + pathToSassjsLoader + '!' + pathToEntrySass,
+                output: {path: filePath} 
+            });
+        })
+        .then(function(stats) {
+            const expectedContents = fs.readFileSync(path.join(__dirname, 'expected', 'basic.css'), 'utf8');
 
-        t.is(stats.toString(), expectedContents);
-    }).catch(function(err) {
-        t.fail(err);
-    });
+            t.is(stats.toString(), expectedContents);
+        }).catch(function(err) {
+            t.fail(err);
+        });
 });
