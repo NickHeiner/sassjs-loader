6eee424f802364948a9fb7779d0babd622456557
Nicholas Heiner <nicholas_t_heiner@omb.eop.gov>
2016-11-04 10:46:10 -0400

    adding failing test for imports

--- test/index.js
+++ test/index.js
@@ -1,71 +1,16 @@
 'use strict';
 
 const test = require('ava'),
-    path = require('path'),
-    webpack = require('webpack'),
-    q = require('q'),
-    fs = require('fs'),
-    tmp = require('tmp'),
-    _ = require('lodash'),
-    cssParse = require('css-parse'),
-    traverse = require('traverse'),
-    
-    packageJson = require('../package'),
-    pathToSassjsLoader = path.join(__dirname, '..', packageJson.main),
-    pathToEntrySass = path.join(__dirname, 'fixtures', 'basic.scss');
+    assertCssEqual = require('./utils/assert-css-equal');
 
 test('Basic sass', function(t) {
     t.plan(1);
 
-    function validateCssAst(cssString) {
-        try {
-            return removePosition(cssParse(cssString.replace('\\n', '\n')));
-        } catch (e) {
-            t.fail('Could not parse css: "' + cssString + '" because of error "' + e + '"');
-        }
-    }
-
-    return q.ninvoke(tmp, 'dir')
-        .then(function(filePath) {
-            return q.nfcall(webpack, {
-                entry: 'raw!' + pathToSassjsLoader + '!' + pathToEntrySass,
-                output: {filename: path.basename(filePath), path: path.dirname(filePath)} 
-            });
-        })
-        .then(function(stats) {
-            if (stats.hasErrors()) {
-                const err = new Error('Webpack compile error');
-                err.compilationErrors = stats.compilation.errors;
-                throw err;
-            }
-
-            if (stats.hasWarnings()) {
-                const err = new Error('Webpack compile warning');
-                err.compilationWarnings = stats.compilation.warnings;
-                throw err;
-            }
-
-            const actual = stats.toJson().modules[0].source, 
-                actualCss = actual.slice(18, -3), // hackity hack hack
-                expectedCss = fs.readFileSync(path.join(__dirname, 'expected', 'basic.css'), 'utf8');
-
-            t.deepEqual(validateCssAst(actualCss), validateCssAst(expectedCss));
-        }).catch(function(err) {
-            const errorMessage = _([err, err.compilationErrors, err.compilationWarnings])
-                .compact()
-                .map(function(errPart) {
-                    return errPart.toString() 
-                })
-                .join('\n');
-
-            t.fail(errorMessage);
-        });
+    return assertCssEqual(t, 'basic');    
 });
 
-function removePosition(cssAst) {
-    return traverse(cssAst).map(function() {
-        if (this.key === 'position') {
-            this.remove(true);
-        }
-    });
-}
+test('Imports', function(t) {
+    t.plan(1);
+
+    return assertCssEqual(t, 'import');    
+});
