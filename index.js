e5c361c49e95fbca70b3732da3d111e376205191
Nicholas Heiner <nicholas_t_heiner@omb.eop.gov>
2016-11-04 11:19:05 -0400

    well, at least some import works

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 'use strict';
 
-var sassJs = require('sass.js');
+var sassJs = require('sass.js'),
+    fs = require('fs'),
+    path = require('path');
 
 module.exports = function(content) {
     var callback = this.async();
-    sassJs.compile(content, function(result) {
+
+    sassJs.importer(function(request, done) {
+        console.log('importer', request);
+        var pathToRead = path.join(__dirname, 'test', 'fixtures', '_imported.scss');
+        done({
+            path: pathToRead,
+            content: fs.readFileSync(pathToRead, 'utf8')
+        });
+    });
+
+    sassJs.compile(content, {inputPath: this.resourcePath}, function(result) {
         if (!result.status) {
             callback(null, result.text);
         } else {
