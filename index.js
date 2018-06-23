15f962d480d1d3b12422f128940c40cd6d059de8
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-06-23 18:11:08 +0200

    Replace gfs with fs implementation. Add dependencies to scss files for hot reloading

--- index.js
+++ index.js
@@ -2,46 +2,48 @@
 
 var sassJs = require('sass.js'),
     q = require('q'),
-    qFs = require('q-io/fs'),
     _ = require('lodash'),
     path = require('path'),
-    
+
     getNodeModuleDir = require('./lib/get-node-module-dir'),
     getResolvedPath = require('./lib/get-resolved-path'),
-    getPathVariations = require('./lib/get-path-variations');
+    getPathVariations = require('./lib/get-path-variations'),
+    readFile = require('./lib/read-file');
 
-module.exports = function(content) {
+module.exports = function (content) {
     var callback = this.async();
+    var self = this;
 
-    sassJs.importer(function(request, done) {
+    sassJs.importer(function (request, done) {
         // Adapted from
         // eslint-disable-next-line max-len 
         // https://github.com/amiramw/grunt-contrib-sassjs/blob/a65f869df967a4e417c4260fd93239e4f0bc55ee/tasks/sass.js#L11
-		if (request.path) {
-			done();
-		} else if (request.resolved) {
-			var resolvedPath = getResolvedPath(request),
+        if (request.path) {
+            done();
+        } else if (request.resolved) {
+            var resolvedPath = getResolvedPath(request),
                 pathVariations = getPathVariations(resolvedPath),
-            
+
                 ostensibleNodeModuleName = _.first(request.current.split(path.sep)),
                 rootNodeModulesDir = getRootNodeModulesDir(ostensibleNodeModuleName);
 
             if (rootNodeModulesDir) {
                 Array.prototype.push.apply(
-                    pathVariations, 
+                    pathVariations,
                     getPathVariations(path.join(rootNodeModulesDir, request.current))
                 );
             }
 
-            q.all(_.map(pathVariations, function(pathVariation) {
-                return qFs.read(pathVariation)
-                    .then(function(fileContents) {
+            q.all(_.map(pathVariations, function (pathVariation) {
+                self.addDependency(pathVariation);
+                return readFile(pathVariation)
+                    .then(function (fileContents) {
                         return {
                             path: pathVariation,
                             content: fileContents
                         };
                     })
-                    .catch(function(err) {
+                    .catch(function (err) {
                         /**
                          * ENOENT happens when the file does not exist. That is expected,
                          * because we only expect one of the many path variations to exist.
@@ -53,18 +55,18 @@ module.exports = function(content) {
                             return null;
                         }
                         throw err;
-                    });    
-            })).then(function(files) {
+                    });
+            })).then(function (files) {
                 done(_(files).compact().first());
-            }).catch(function(err) {
-                done({error: JSON.stringify(err)});
+            }).catch(function (err) {
+                done({ error: JSON.stringify(err) });
             });
-		} else {
-			done();
-		}
+        } else {
+            done();
+        }
     });
 
-    sassJs.compile(content, {inputPath: this.resourcePath}, function(result) {
+    sassJs.compile(content, { inputPath: this.resourcePath }, function (result) {
         if (!result.status) {
             callback(null, result.text);
         } else {
